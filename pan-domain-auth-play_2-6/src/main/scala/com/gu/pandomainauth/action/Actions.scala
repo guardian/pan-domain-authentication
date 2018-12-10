58179dce6af96562faac397494c3f6b132f155e1
Michael Barton <michael.barton@guardian.co.uk>
2018-12-10 13:55:07 +0000

    Remove hard-coded validateUser function
    The `guardianValidation` function remains should people which to use it

--- pan-domain-auth-play_2-6/src/main/scala/com/gu/pandomainauth/action/Actions.scala
+++ pan-domain-auth-play_2-6/src/main/scala/com/gu/pandomainauth/action/Actions.scala
@@ -205,7 +205,7 @@ trait AuthActions {
     */
   def extractAuth(request: RequestHeader): AuthenticationStatus = {
     readCookie(request).map { cookie =>
-      PanDomain.authStatus(cookie.value, settings.publicKey) match {
+      PanDomain.authStatus(cookie.value, settings.publicKey, validateUser) match {
         case Expired(authedUser) if authedUser.isInGracePeriod(apiGracePeriod) =>
           GracePeriod(authedUser)
         case authStatus @ Authenticated(authedUser) =>
