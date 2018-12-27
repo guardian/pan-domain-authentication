5ef135bda1649200b3c8929657e760e2df3670ab
Mariot Chauvin <mariot.chauvin@guardian.co.uk>
2018-12-27 17:30:12 +0000

    Update bouncy castle to latest version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
   )
 
   val cryptoDependencies = Seq(
-    "org.bouncycastle" % "bcprov-jdk15on" % "1.58",
+    "org.bouncycastle" % "bcprov-jdk15on" % "1.60",
     "commons-codec" % "commons-codec" % "1.10"
   )
 
