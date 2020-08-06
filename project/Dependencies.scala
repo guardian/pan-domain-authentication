d03a1103c00c5d5fafc4f9c6c906c2bcda0678ed
Michael Barton <michael.barton@guardian.co.uk>
2020-08-06 12:10:33 +0100

    Bump versions again to fix sbt-dependency-graph

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,10 @@ import sbt._
 
 object Dependencies {
 
-  val awsDependencies = Seq("com.amazonaws" % "aws-java-sdk-s3" % "1.11.787")
+  val awsDependencies = Seq("com.amazonaws" % "aws-java-sdk-s3" % "1.11.835")
 
   val playLibs_2_6 = {
-    val version = "2.6.11"
+    val version = "2.6.25"
     Seq(
       "com.typesafe.play" %% "play" % version % "provided",
       "com.typesafe.play" %% "play-ws" % version % "provided"
@@ -13,7 +13,7 @@ object Dependencies {
   }
 
   val playLibs_2_7 = {
-    val version = "2.7.1"
+    val version = "2.7.5"
     Seq(
       "com.typesafe.play" %% "play" % version % "provided",
       "com.typesafe.play" %% "play-ws" % version % "provided"
@@ -21,7 +21,7 @@ object Dependencies {
   }
 
   val playLibs_2_8 = {
-    val version = "2.8.1"
+    val version = "2.8.2"
     Seq(
       "com.typesafe.play" %% "play" % version % "provided",
       "com.typesafe.play" %% "play-ws" % version % "provided"
@@ -29,23 +29,23 @@ object Dependencies {
   }
 
   val googleDirectoryApiDependencies = Seq(
-    "com.google.auth" % "google-auth-library-oauth2-http" % "0.20.0",
+    "com.google.auth" % "google-auth-library-oauth2-http" % "0.21.1",
     "com.google.apis" % "google-api-services-admin-directory" % "directory_v1-rev118-1.25.0"
   )
 
   val cryptoDependencies = Seq(
-    "org.bouncycastle" % "bcprov-jdk15on" % "1.65",
+    "org.bouncycastle" % "bcprov-jdk15on" % "1.66",
     "commons-codec" % "commons-codec" % "1.14"
   )
 
-  val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.1.2" % "test")
+  val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.2.0" % "test")
 
   /*
   * Pull in an updated version of jackson and logback libraries as the ones AWS use have security vulnerabilities.
   * See https://github.com/aws/aws-sdk-java/pull/1373
   * */
   val jackson: Seq[ModuleID] = {
-    val version = "2.11.0"
+    val version = "2.11.2"
     Seq(
       "com.fasterxml.jackson.core" % "jackson-core" % version,
       "com.fasterxml.jackson.core" % "jackson-databind" % version,
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,12 +5,12 @@ logLevel := Level.Warn
 resolvers += "Typesafe repository" at "https://repo.typesafe.com/typesafe/releases/"
 
 // Use the Play sbt plugin for Play projects
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.1")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.2")
 
 addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.13")
 
-addSbtPlugin("com.jsuereth" % "sbt-pgp" % "2.0.0")
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "2.0.1")
 
-addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "3.9.2")
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "3.9.4")
 
-addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.9.2")
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.10.0-RC1")
