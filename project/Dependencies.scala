df9a4ef81f5a0589dc992659189ffcfb883a422d
Pete F <pete.faulconbridge@theguardian.com>
2024-07-08 11:59:41 +0100

    Remove manual override for google-oauth-client
    The direct google dependencies of this project now include a version higher than the manually overridden one, which avoids the vulnerability the override was added to address.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,12 +36,6 @@ object Dependencies {
     "com.google.apis" % "google-api-services-admin-directory" % "directory_v1-rev20230124-2.0.0",
     "com.google.auth" % "google-auth-library-credentials" % "1.16.0",
     "com.google.auth" % "google-auth-library-oauth2-http" % "1.16.0",
-    /*
-     * Normally transitive from the above, pull up manually to fix:
-        - https://app.snyk.io/vuln/SNYK-JAVA-COMGOOGLEOAUTHCLIENT-575276
-        - https://security.snyk.io/vuln/SNYK-JAVA-COMGOOGLEOAUTHCLIENT-2807808
-     */
-    "com.google.oauth-client" % "google-oauth-client" % "1.33.3"
   )
 
   val cryptoDependencies = Seq(
