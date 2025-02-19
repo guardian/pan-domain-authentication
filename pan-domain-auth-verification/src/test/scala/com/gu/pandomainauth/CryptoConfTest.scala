8907be9787e1def3f6a9b94f8eeb577fb82e3abe
Roberto Tyley <52038+rtyley@users.noreply.github.com>
2025-02-19 16:26:29 +0000

    Add non-active-key monitoring for key-rotation

--- pan-domain-auth-verification/src/test/scala/com/gu/pandomainauth/CryptoConfTest.scala
+++ pan-domain-auth-verification/src/test/scala/com/gu/pandomainauth/CryptoConfTest.scala
@@ -1,40 +1,67 @@
 package com.gu.pandomainauth
 
+import com.gu.pandomainauth.SampleConf.loadExample
+import com.gu.pandomainauth.service.CryptoConf
 import com.gu.pandomainauth.service.CryptoConf.{SettingsReader, SigningAndVerification}
 import com.gu.pandomainauth.service.TestKeys.testPublicKey
-import org.scalatest.EitherValues
+import org.scalatest.{EitherValues, OptionValues}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 import java.nio.charset.StandardCharsets.UTF_8
 
+object SampleConf {
+  def loadExample(name: String): SigningAndVerification = {
+    val settingsText =
+      new String(getClass.getResourceAsStream(s"/crypto-conf-rotation-example/$name.settings").readAllBytes(), UTF_8)
+    SettingsReader(Settings.extractSettings(settingsText).toOption.get).signingAndVerificationConf.toOption.get
+  }
+}
 
-class CryptoConfTest extends AnyFreeSpec with Matchers with EitherValues {
+class CryptoConfTest extends AnyFreeSpec with Matchers with EitherValues with OptionValues {
+  val legacyConf = loadExample("0.legacy")
+  val rotationUpcomingConf = loadExample("1.rotation-upcoming")
+  val rotationInProgressConf = loadExample("2.rotation-in-progress")
+  val rotationCompleteConf = loadExample("3.rotation-complete")
+
   "loading crypto configuration" - {
     "follow a safe set of transition steps" in {
-      val legacyConf = loadExample("0.legacy")
       legacyConf.alsoAccepted shouldBe empty
 
-      val rotationUpcomingConf = loadExample("1.rotation-upcoming")
       rotationUpcomingConf.activeKeyPair should === (legacyConf.activeKeyPair)
       rotationUpcomingConf.alsoAccepted should not be empty
       val expectedAcceptedPublicKeys = rotationUpcomingConf.activeKeyPair.publicKey +: rotationUpcomingConf.alsoAccepted
       rotationUpcomingConf.acceptedPublicKeys should === (expectedAcceptedPublicKeys)
 
-      val rotationInProgressConf = loadExample("2.rotation-in-progress")
       rotationInProgressConf.activeKeyPair should !== (legacyConf.activeKeyPair)
       rotationInProgressConf.alsoAccepted shouldBe Seq(legacyConf.activeKeyPair.publicKey)
 
-      val rotationCompleteConf = loadExample("3.rotation-complete")
       rotationCompleteConf.activeKeyPair should === (rotationInProgressConf.activeKeyPair)
       rotationCompleteConf.alsoAccepted shouldBe empty
     }
-  }
 
-  private def loadExample(name: String): SigningAndVerification = {
-    val settingsText =
-      new String(getClass.getResourceAsStream(s"/crypto-conf-rotation-example/$name.settings").readAllBytes(), UTF_8)
-    SettingsReader(Settings.extractSettings(settingsText).value).signingAndVerificationConf.value
+    "have transitions that are reported as safe" in {
+      val stages = Seq(legacyConf, rotationUpcomingConf, rotationInProgressConf, rotationCompleteConf)
+      for {
+        (before, after) <- stages.zip(stages.tail)
+      } {
+        val change = CryptoConf.Change.compare(before, after).value
+        println(change.summary)
+        change.isBreakingChange shouldBe false
+      }
+    }
+
+    "report a bad transition if the old active key is not still tolerated" in {
+      val change = CryptoConf.Change.compare(rotationUpcomingConf, rotationCompleteConf).value
+      println(change.summary)
+      change.isBreakingChange shouldBe true
+    }
+
+    "report a bad transition if the new active key wasn't already accepted by the old config" in {
+      val change = CryptoConf.Change.compare(legacyConf, rotationInProgressConf).value
+      println(change.summary)
+      change.isBreakingChange shouldBe true
+    }
   }
 
   "CryptoConf.SettingsReader" - {
