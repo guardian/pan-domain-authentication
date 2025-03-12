c591fabeae7773e78a9642557a2314207a58e672
Joseph Smith <joseph.lochlann@gmail.com>
2025-03-12 17:44:53 +0000

    Add comment with isFresh/isAcceptable thinking

--- pan-domain-auth-verification/src/main/scala/com/gu/pandomainauth/model/AuthenticatedUser.scala
+++ pan-domain-auth-verification/src/main/scala/com/gu/pandomainauth/model/AuthenticatedUser.scala
@@ -5,6 +5,13 @@ import scala.math.Ordering.Implicits._
 
 case class AuthenticatedUser(user: User, authenticatingSystem: String, authenticatedIn: Set[String], expires: Instant, multiFactor: Boolean) {
 
+  // **WIP thinking**
+  //
+  // The following structure might be better:
+  // case class CookieStatus(isFresh: Boolean, isAcceptable: Boolean)
+  // issued ------------------------- fresh period over -------------------- grace period over
+  // [--working-and-will-not-renew--------][---working-and-will-renew--------------------------][--not-working---]
+
   def isExpired = Instant.now() > expires
   def isInGracePeriod(period: Duration) = Instant.now() < (expires plus period)
 }
