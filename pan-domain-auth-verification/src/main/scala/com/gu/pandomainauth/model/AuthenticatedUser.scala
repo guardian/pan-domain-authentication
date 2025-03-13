7c1b21dbf7fcd51bfe974bfecbfc2023b7c3bda8
Joseph Smith <joseph.lochlann@gmail.com>
2025-03-13 14:58:10 +0000

    Remove WIP comments

--- pan-domain-auth-verification/src/main/scala/com/gu/pandomainauth/model/AuthenticatedUser.scala
+++ pan-domain-auth-verification/src/main/scala/com/gu/pandomainauth/model/AuthenticatedUser.scala
@@ -5,14 +5,6 @@ import java.time.{Duration, Instant}
 import scala.math.Ordering.Implicits._
 
 case class AuthenticatedUser(user: User, authenticatingSystem: String, authenticatedIn: Set[String], expires: Instant, multiFactor: Boolean) {
-
-  // **WIP thinking**
-  //
-  // The following structure might be better:
-  // case class CookieStatus(isFresh: Boolean, isAcceptable: Boolean)
-  // issued ------------------------- fresh period over -------------------- grace period over
-  // [--working-and-will-not-renew--------][---working-and-will-renew--------------------------][--not-working---]
-
   def isExpired = now() > expires
   def isInGracePeriod(period: Duration) = now() < (expires plus period)
 }
