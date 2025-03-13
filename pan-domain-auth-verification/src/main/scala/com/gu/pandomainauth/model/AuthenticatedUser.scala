89a7a4b30c4086f4a043445fafb44dd29d5b018a
Joseph Smith <joseph.lochlann@gmail.com>
2025-03-13 15:06:22 +0000

    Restore whitespace

--- pan-domain-auth-verification/src/main/scala/com/gu/pandomainauth/model/AuthenticatedUser.scala
+++ pan-domain-auth-verification/src/main/scala/com/gu/pandomainauth/model/AuthenticatedUser.scala
@@ -5,6 +5,7 @@ import java.time.{Duration, Instant}
 import scala.math.Ordering.Implicits._
 
 case class AuthenticatedUser(user: User, authenticatingSystem: String, authenticatedIn: Set[String], expires: Instant, multiFactor: Boolean) {
+
   def isExpired = now() > expires
   def isInGracePeriod(period: Duration) = now() < (expires plus period)
 }
