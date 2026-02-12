66c0d8b952c4183cff8756df30c276ffbbbc9abf
Roberto Tyley <roberto.tyley@gmail.com>
2026-02-12 17:04:25 +0000

    upickle seems to require keys to be present, even if they're Options, unless they have default values

--- pan-domain-auth-core/src/main/scala/com/gu/pandomainauth/oauth/UserInfo.scala
+++ pan-domain-auth-core/src/main/scala/com/gu/pandomainauth/oauth/UserInfo.scala
@@ -5,8 +5,8 @@ import upickle.default.*
 /*
  * https://www.oauth.com/oauth2-servers/signing-in-with-google/verifying-the-user-info/
  */
-case class UserInfo(sub: Option[String], name: String, given_name: String, family_name: String, profile: Option[String],
-  picture: Option[String], email: String, locale: Option[String], hd: Option[String])
+case class UserInfo(sub: Option[String], name: String, given_name: String, family_name: String, profile: Option[String] = None,
+  picture: Option[String] = None, email: String, locale: Option[String] = None, hd: Option[String] = None)
 
 object UserInfo {
   implicit val userInfoRW: ReadWriter[UserInfo] = macroRW[UserInfo]
