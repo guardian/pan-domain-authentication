c6ddb25e93ca0d55e8f9ca651864290167ffe206
Roberto Tyley <roberto.tyley@gmail.com>
2026-03-12 10:43:56 +0000

    Ensure that 'authenticatedIn' is updated on OAuth-callback

---
+++ pan-domain-auth-core/src/main/scala/com/gu/pandomainauth/oauth/NewlyOAuthedUserHandler.scala
@@ -0,0 +1,30 @@
+package com.gu.pandomainauth.oauth
+
+import cats.*
+import cats.syntax.all.*
+import com.gu.pandomainauth.*
+import com.gu.pandomainauth.internal.planning.*
+import com.gu.pandomainauth.model.AuthenticatedUser
+import com.gu.pandomainauth.service.TwoFactorAuthChecker
+
+import java.net.URI
+
+class NewlyOAuthedUserHandler[F[_] : Monad](systemAuthorisation: SystemAuthorisation, twoFactorAuthChecker: Option[TwoFactorAuthChecker[F]]) {
+
+  private def augmentWithMultiFactor(newlyClaimedAuth: AuthenticatedUser): F[AuthenticatedUser] =
+    twoFactorAuthChecker.traverse(_.check(newlyClaimedAuth.user.email)).map { multiFactorOpt =>
+      newlyClaimedAuth.copy(multiFactor = multiFactorOpt.getOrElse(false))
+    }
+
+  def planFor(newlyClaimedAuth: AuthenticatedUser, priorAuth: AuthPersistenceStatus, returnUrl: URI): F[Plan[OAuthCallbackEndpoint.RespType, OAuthCallbackEndpoint.RespMod]] = for {
+    userWithMultifactorStatus <- augmentWithMultiFactor(newlyClaimedAuth)
+  } yield {
+    if (systemAuthorisation.isAuthorised(newlyClaimedAuth, disableCache = true))
+      Plan(Redirect(returnUrl), Some(PersistAuth(
+        userWithMultifactorStatus
+          .withAuthorisationIn(systemAuthorisation.system)
+          .augmentWith(priorAuth.effectiveAuthStatus),
+        wipeTemporaryCookiesUsedForOAuth = true)))
+    else Plan(NotAuthorized(newlyClaimedAuth))
+  }
+}
