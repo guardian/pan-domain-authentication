c7fdcf9fcc0075c35b4e0a746099a033620506a4
Roberto Tyley <roberto.tyley@gmail.com>
2026-03-25 18:17:27 +0000

    Fix OAuthCallback authorisation so that it gets the actual multifactor status

--- pan-domain-auth-core/src/test/scala/com/gu/pandomainauth/oauth/NewlyOAuthedUserHandlerTest.scala
+++ pan-domain-auth-core/src/test/scala/com/gu/pandomainauth/oauth/NewlyOAuthedUserHandlerTest.scala
@@ -3,7 +3,7 @@ package com.gu.pandomainauth.oauth
 import cats.*
 import com.gu.pandomainauth.SystemAuthorisation
 import com.gu.pandomainauth.internal.planning
-import com.gu.pandomainauth.internal.planning.AuthPersistenceStatus
+import com.gu.pandomainauth.internal.planning.{AuthPersistenceStatus, NotAuthorized, Redirect}
 import com.gu.pandomainauth.model.{AuthenticatedUser, NotAuthenticated, User}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
@@ -13,7 +13,8 @@ import java.net.URI
 import java.time.Instant
 
 class NewlyOAuthedUserHandlerTest extends AnyFreeSpec with Matchers with OptionValues with Inside {
-  val initialAuthedUser = AuthenticatedUser(User("John", "Smith", "", None), "composer", Set.empty, Instant.now().plusSeconds(1), true)
+  val initialAuthedUserWithoutMultifactorStatus =
+    AuthenticatedUser(User("John", "Smith", "", None), "composer", Set.empty, Instant.now().plusSeconds(1), false)
 
   val examplePageRequestUrl: URI = URI.create("https://example.tool.co.uk/admin")
 
@@ -24,14 +25,28 @@ class NewlyOAuthedUserHandlerTest extends AnyFreeSpec with Matchers with OptionV
         twoFactorAuthChecker = None
       )
 
-      initialAuthedUser.authenticatedIn shouldBe empty // authorisation has not happened yet
+      initialAuthedUserWithoutMultifactorStatus.authenticatedIn shouldBe empty // authorisation has not happened yet
 
       val plan =
-        handler.planFor(initialAuthedUser, priorAuth = AuthPersistenceStatus(NotAuthenticated, Set.empty), returnUrl = examplePageRequestUrl)
+        handler.planFor(initialAuthedUserWithoutMultifactorStatus, priorAuth = AuthPersistenceStatus(NotAuthenticated, Set.empty), returnUrl = examplePageRequestUrl)
 
       inside (plan.respMod.value) {
         case planning.PersistAuth(persistedUser, _) => persistedUser.authenticatedIn shouldBe Set("composer")
       }
+    }
+
+    "ensure multi-factor status is available to authorisation checks" in {
+      def responseWhen2FACheckReturns(status: Boolean) = new NewlyOAuthedUserHandler[Id](
+          SystemAuthorisation("composer", _.multiFactor, false),
+          twoFactorAuthChecker = Some(_ => status)
+        ).planFor(
+          initialAuthedUserWithoutMultifactorStatus,
+          priorAuth = AuthPersistenceStatus(NotAuthenticated, Set.empty),
+          returnUrl = examplePageRequestUrl
+        ).respType
+
+      responseWhen2FACheckReturns(true) shouldBe Redirect(examplePageRequestUrl)
+      responseWhen2FACheckReturns(false) shouldBe NotAuthorized(initialAuthedUserWithoutMultifactorStatus)
     }
   }
 }
